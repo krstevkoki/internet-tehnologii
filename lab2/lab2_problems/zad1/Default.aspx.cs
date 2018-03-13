1651312761d80c243040c08e2387898ce0654876
Kostadin Krstev <krstev.kostadin@gmail.com>
2018-03-13 15:27:05 +0100

    fixed typo

--- lab2/lab2_problems/zad1/Default.aspx.cs
+++ lab2/lab2_problems/zad1/Default.aspx.cs
@@ -20,7 +20,7 @@ public partial class _Default : System.Web.UI.Page
         "Септември",
         "Октомври",
         "Ноември",
-        "Демекври"
+        "Декември"
     };
 
     protected void Page_Load(object sender, EventArgs e)
