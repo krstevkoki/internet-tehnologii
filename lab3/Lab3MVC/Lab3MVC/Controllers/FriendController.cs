3d27b7b8f4c572bcc8fb647dae0a8ba21746230a
Kostadin Krstev <krstev.kostadin@gmail.com>
2018-04-08 20:18:28 +0200

    Initial commit

---
+++ lab3/Lab3MVC/Lab3MVC/Controllers/FriendController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Lab3MVC.Models;
+
+namespace Lab3MVC.Controllers
+{
+    public class FriendController : Controller
+    {
+        private static List<FriendModel> friends = new List<FriendModel>()
+        {
+            new FriendModel()
+            {
+                FriendID = 0,
+                Name = "Kostadin Krstev",
+                Hometown = "Negotino"
+            }
+        };
+
+        // GET: Friend
+        public ActionResult AllFriends()
+        {
+            return View(friends);
+        }
+
+        public ActionResult AddFriend()
+        {
+            FriendModel model = new FriendModel();
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult AddFriend(FriendModel model)
+        {
+            if (!ModelState.IsValid)
+                return View("AddFriend", model);
+            friends.Add(model);
+            return RedirectToAction("AllFriends", "Friend");
+        }
+
+        public ActionResult EditFriend(int id)
+        {
+            var model = friends.ElementAt(id);
+            model.ID = id;
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult EditFriendPost(FriendModel model)
+        {
+            if (!ModelState.IsValid)
+                return View("EditFriend", model);
+            friends.RemoveAt(model.ID);
+            friends.Insert(model.ID, model);
+            return RedirectToAction("AllFriends", "Friend");
+        }
+
+        public ActionResult DeleteFriend(int id)
+        {
+            friends.RemoveAt(id);
+            return RedirectToAction("AllFriends", "Friend");
+        }
+    }
+}
---
+++ lab3/Lab3MVC/Lab3MVC/Models/FriendModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data;
+using System.Linq;
+using System.Web;
+
+namespace Lab3MVC.Models
+{
+    public class FriendModel
+    {
+        public int ID { get; set; }
+
+        [Required]
+        [Range(0, 200, ErrorMessage = "FriendID must be between 0-200")]
+        [Display(Name = "Friend ID")]
+
+        public int FriendID { get; set; }
+
+        [Required] public string Name { get; set; }
+        [Required] public string Hometown { get; set; }
+    }
+}
