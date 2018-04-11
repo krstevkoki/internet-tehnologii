eda8b1e74de4911d99046d7ca354552a6f798d6c
Kostadin Krstev <krstev.kostadin@gmail.com>
2018-04-11 12:52:50 +0200

    Add models

---
+++ aud6/Aud3MVC/Aud3MVC/Models/Client.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Aud3MVC.Models
+{
+    public class Client
+    {
+        [Required] public string Name { get; set; }
+        [Required] public string Address { get; set; }
+
+        [Required]
+        [Range(1, Int32.MaxValue, ErrorMessage = "Age must be a positive number (> 0)")]
+        public int Age { get; set; }
+
+        [Required]
+        [Display(Name = "Member card")]
+        public string MemberCard { get; set; }
+
+        [Required] public string Telephone { get; set; }
+        public bool IsSubscribed { get; set; }
+    }
+}
