4d7916542d50b77a68bd8babafcf87694be34613
Kostadin Krstev <krstev.kostadin@gmail.com>
2018-04-28 18:42:06 +0200

    Add EntityFramework, DataTables, WebAPI

--- lab4/Lab4MVC/Lab4MVC/Global.asax.cs
+++ lab4/Lab4MVC/Lab4MVC/Global.asax.cs
@@ -5,13 +5,15 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using System.Web.Http;
 
 namespace Lab4MVC
 {
-    public class MvcApplication : System.Web.HttpApplication
+    public class MvcApplication : HttpApplication
     {
         protected void Application_Start()
         {
+            GlobalConfiguration.Configure(WebApiConfig.Register);
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
